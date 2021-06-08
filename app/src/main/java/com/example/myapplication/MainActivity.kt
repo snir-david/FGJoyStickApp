891a4231f58efe547c6d6694c31f8a1e145b70c8
Snir David Nahari <snir-david@users.noreply.github.com>
2021-06-08 21:52:03 +0300

    project process

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,75 @@
 package com.example.myapplication
 
-import android.graphics.Color
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("MainActivity", "app Started")
         val vm = ViewModelProvider(this).get(ViewModel::class.java)
-        binding = DataBindingUtil.setContentView<ActivityMainBinding?>(this, R.layout.activity_main
+        binding = DataBindingUtil.setContentView<ActivityMainBinding?>(
+            this, R.layout.activity_main
         ).apply {
-        this.lifecycleOwner = this@MainActivity
-        this.viewModel = vm
+            this.lifecycleOwner = this@MainActivity
+            this.viewModel = vm
         }
         vm.binding = binding
+        Log.i("MainActivity", "x: ${binding.joystickCenter.x}, y: ${binding.joystickCenter.y}")
+        this.binding.joystickCenter.setOnTouchListener { v, event ->
+            var x = 0F
+            var y = 0F
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Log.i("MainActivity", "image Position is : ${binding.joystickCenter.x} , ${binding.joystickCenter.y} and View is : ${v.x}, ${v.y}")
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    x = event.x
+                    y = event.y
+                    v.animate().x(event.rawX).y(event.rawY).setDuration(0).start();
+//                    vm.joyStickOnChanged(x, y)
+                    Log.i("MainActivity", "Move coordination is : $x , $y and View is : ${v.x}, ${v.y}")
+                }
+            }
+            true
+        }
+        throttleBinding()
+    }
+
+    private fun throttleBinding() {
+        binding.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seek: SeekBar,
+                progress: Int, fromUser: Boolean
+            ) {
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {}
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is stopped
+                Toast.makeText(
+                    this@MainActivity,
+                    "Progress is: " + seek.progress + "%",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 
 }
+
--- app/src/main/java/com/example/myapplication/ViewModel.kt
+++ app/src/main/java/com/example/myapplication/ViewModel.kt
@@ -1,21 +1,20 @@
 package com.example.myapplication
 
-import android.opengl.Visibility
-import android.os.StrictMode
-import android.os.StrictMode.ThreadPolicy
+import android.os.Build
 import android.util.Log
 import android.view.View
-import androidx.lifecycle.LiveData
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.myapplication.databinding.ActivityMainBinding
-import java.net.InetSocketAddress
-import java.net.Socket
 
 
 class ViewModel() : ViewModel() {
     val ip = MutableLiveData<String>()
     val port = MutableLiveData<String>()
+    val throttle = MutableLiveData<String>()
+    var joyStickX: Float = 0.0f
+    var joyStickY: Float = 0.0f
     private var Model: Model = Model()
     lateinit var binding: ActivityMainBinding
 
@@ -30,4 +29,28 @@ class ViewModel() : ViewModel() {
         modelThread.start()
     }
 
+    fun onThrottleChanged() {
+
+    }
+
+    fun joyStickOnChanged(x: Float, y: Float) {
+        val posXY = IntArray(2)
+        binding.joystickCenter.getLocationInWindow(posXY)
+        Log.i("ViewModel", "x: ${posXY[0]}  y: ${posXY[1]}")
+        if (isValid(x, y)) {
+            joyStickX = x
+            joyStickY = y
+            binding.joystickCenter.x = x
+            binding.joystickCenter.y = y
+        }
+    }
+
+    private fun isValid(x: Float, y: Float): Boolean {
+        if (x < -345 || x > 720)
+            return false
+        if (y < -345 || y>720)
+            return false
+        return true
+    }
 }
+
